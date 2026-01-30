36e107e36abff8d3966fb903a2358a75c649b9b8
José Luis González Sánchez <joseluisgonsan@gmail.com>
2026-01-30 12:39:12 +0100

    fix(graphql): añadir paquete y configuración de autorización faltante
    - Se añade el paquete HotChocolate.AspNetCore.Authorization.
    - Se activa el middleware de autorización en la infraestructura de GraphQL.
    - Corrige errores en tiempo de ejecución al usar atributos [Authorize] en el esquema.

--- TiendaApi.Api/Infrastructures/GraphQLConfig.cs
+++ TiendaApi.Api/Infrastructures/GraphQLConfig.cs
@@ -22,6 +22,7 @@ public static class GraphQLConfig
         Log.Information("🔍 Configurando GraphQL con HotChocolate...");
         return services
             .AddGraphQLServer()
+            .AddAuthorization()
             .AddQueryType<TiendaQuery>()
             .AddMutationType<ProductoMutation>()
             .AddSubscriptionType<ProductoSubscription>()
